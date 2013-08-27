4a09438a6dd30f7f15467fc1529332fccfa66a85
René Pedersen <rkhpedersen@gmail.com>
2013-08-27 16:30:39 +0200

    René's first commit :-)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /**
  * Created with JetBrains WebStorm.
- * User: René Pede
+ * User: René Peder
  * Date: 27/08/13
  * Time: 15:57
  * To change this template use File | Settings | File Templates.
