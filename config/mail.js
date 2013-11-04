cde4e461bdd84b9c1c2b2294be6d0ba33ea7a893
Etienne <etienne.dodat@gmail.com>
2013-11-04 14:31:15 +0100

    added Nodemailer dependency

---
+++ config/mail.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    transportOptions : {
+        service: "Gmail",
+        auth: {
+            user: "etienne.dodat@gmail.com",
+            pass: "fggja7gq"
+        }
+    },
+
+    PRODUCT: 'Lean Project',
+    FROM: 'Lean Project <no-reply@lean-project.com>',
+    SUPPORT: 'support@lean-project.com'
+
+};
